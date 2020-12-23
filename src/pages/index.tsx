caa00a463b5c2dc97bbca02863fa14a2565e1cc3
LeleoSouza <leobrausa@hotmail.com>
2020-12-23 02:34:18 -0500

    stucture

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import  Resume  from "./resume/index";
+function Pages() {
+  return (
+    <div className="pages">
+     <Resume></Resume>
+    </div>
+  );
+}
+
+export default Pages;
---
+++ src/pages/resume/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <div className="pages">
+     <a>
+         TEST
+     </a>
+    </div>
+  );
+}
+
+export default Resume;
