0a6df151fa22d2d86c6b9311e59aa7349fac6988
LeleoSouza <leobrausa@hotmail.com>
2020-12-23 13:31:45 -0500

    added animation

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -1,22 +1,32 @@
+import Typist from "react-typist";
 function Intro(props: any) {
   return (
     <div className="container" style={styles.container}>
-      <h1>INTRO</h1>
+      <Typist>
+        <h1>
+          I'M
+          <br />
+          LEONARDO
+          <br />
+          SOUZA
+        </h1>
+      </Typist>
     </div>
   );
 }
 
 const styles = {
   container: {
-    backgroundColor: "pink",
+    backgroundColor: "gray",
     height: "100vh",
-    color: "black",
+    color: "white",
     width: "70%",
     marginLeft: "15%",
     marginRight: "15%",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
+    fontSize: "30px",
   },
 };
 export default Intro;
